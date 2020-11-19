f6f5b894f7a59dbb2750abcee1c3d27f6c2a9ac3
JeffDinsmore <hello@jeffdinsmore.com>
2020-11-19 13:42:25 -0800

    adding help-queue exercise

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+  return (
+    <React.Fragment>
+    <h1>Help Queue</h1>,
+    <h3>Help me ronday</h3>
+    </React.Fragment>
+  );
+}
+
+export default Header;
