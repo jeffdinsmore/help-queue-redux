26b65552c6e8cb9e0346768023184eb3e68b34c3
JeffDinsmore <hello@jeffdinsmore.com>
2020-12-07 09:25:15 -0800

    added .env to gitignore file

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -2,24 +2,6 @@ import React from "react";
 import Ticket from "./Ticket";
 import PropTypes from "prop-types";
 
-// const masterTicketList = [
-//   {
-//     names: 'Thato and Haley',
-//     location: '3A',
-//     issue: 'Firebase won\'t save record. Halp.'
-//   },
-//   {
-//     names: 'Sleater and Kinney',
-//     location: '4B',
-//     issue: 'Prop types are throwing an error.'
-//   },
-//   {
-//     names: 'Imani & Jacob',
-//     location: '9F',
-//     issue: 'Child component isn\'t rendering.'
-//   }
-// ];
-
 function TicketList(props) {
   return (
     <React.Fragment>
